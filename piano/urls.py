67c9566a9a27ca67ffe2e8a13902f84943f72061
briano <briano.bend@icloud.com>
2020-11-26 06:47:14 -0800

    Testing new url views

--- piano/urls.py
+++ piano/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('page_test', views.page_test, name="example")
 ]
--- piano/views.py
+++ piano/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the piano index.")
+
+def page_test(request):
+	random_number = random.randint(1,100)
+	return HttpResponse(str(random_number))
