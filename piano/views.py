1624cc23f83bec8434f4baf7c7e71e7aeecc66fd
briano <briano.bend@icloud.com>
2020-12-11 08:45:58 -0800

    Tester writes one bar of music xml parsed so that Finale can read it

--- piano/views.py
+++ piano/views.py
@@ -15,14 +15,35 @@ def page_test(request):
 
     top = Element('score-partwise')
 
-    comment = Comment("this is a new comment")
-    top.append(comment)
     top.attrib = {"version": "3.1"}
     part_list = SubElement(top, 'part-list')
-    part_name = SubElement(part_list, 'part_name')
+    score_part = SubElement(part_list, "score-part")
+    score_part.attrib = {"id": "P1"}
+    part_name = SubElement(score_part, 'part-name')
     part_name.text = "MusicXML Part"
+    score_instrument = SubElement(score_part, 'score-instrument')
+    score_instrument.attrib = {"id": "P1-I1"}
+    instrument_name = SubElement(score_instrument, 'instrument-name')
+    instrument_name.text = "Instrument 1"
 
-    print(tostring(top))
+    part = SubElement(top, 'part')
+    part.attrib = {"id":"P1"}
+    measure1 = SubElement(part, 'measure')
+    measure1.attrib = {"number": "1"}
+    note1 = SubElement(measure1, "note")
+    rest = SubElement(note1, "rest")
+    rest.attrib = {"measure": "yes"}
+    duration = SubElement(note1, "duration")
+    duration.text = "8"
+    voice = SubElement(note1, "voice")
+    voice.text = "1"
+
+    barline = SubElement(measure1, "barline")
+    barline.attrib = {"location": "right"}
+    bar_style = SubElement(barline, "bar-style")
+    bar_style.text = "light-heavy"
+
+    #print(tostring(top))
 
     dom = xml.dom.minidom.parseString(tostring(top))
     pretty_xml_as_string = dom.toprettyxml()
