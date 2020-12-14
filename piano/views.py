baa4b45218a5c1c69e879cf0c25e38ae12945615
briano <briano.bend@icloud.com>
2020-12-14 14:54:57 -0800

    Creates 2 bars of random eighth notes

--- piano/views.py
+++ piano/views.py
@@ -4,12 +4,13 @@ from xml.etree.ElementTree import Element, SubElement, Comment, tostring, Elemen
 import xml.dom.minidom
 import random
 
+note_names = ["A", "B", "C", "D", "E", "F", "G"]
 
 def create_xml_stub():
-    top = Element('score-partwise')
+    xml = Element('score-partwise')
 
-    top.attrib = {"version": "3.1"}
-    part_list = SubElement(top, 'part-list')
+    xml.attrib = {"version": "3.1"}
+    part_list = SubElement(xml, 'part-list')
     score_part = SubElement(part_list, "score-part")
     score_part.attrib = {"id": "P1"}
     part_name = SubElement(score_part, 'part-name')
@@ -19,26 +20,53 @@ def create_xml_stub():
     instrument_name = SubElement(score_instrument, 'instrument-name')
     instrument_name.text = "Instrument 1"
 
-    return top
+    return xml
 
-def add_measure_to_xml(xml):
 
-    part = SubElement(xml, 'part')
-    part.attrib = {"id": "P1"}
-    measure1 = SubElement(part, 'measure')
-    measure1.attrib = {"number": "1"}
-    note1 = SubElement(measure1, "note")
+def add_note(measure):
+    note = SubElement(measure, "note")
+    pitch = SubElement(note, "pitch")
+    step = SubElement(pitch, "step")
+    step_choice = random.choice(note_names)
+    step.text = step_choice
+    octave = SubElement(pitch, "octave")
+    octave.text = "4"
+    duration = SubElement(note, "duration")
+    duration.text = "1"
+    voice = SubElement(note, "voice")
+    voice.text = "1"
+    note_type = SubElement(note, "type")
+    note_type.text = "eighth"
+
+
+def add_measure(part, measure_number, final=False):
+    measure = SubElement(part, 'measure')
+    measure.attrib = {"number": str(measure_number)}
+    for i in range(8):
+        add_note(measure)
+    """
+    note1 = SubElement(measure, "note")
     rest = SubElement(note1, "rest")
     rest.attrib = {"measure": "yes"}
     duration = SubElement(note1, "duration")
     duration.text = "8"
     voice = SubElement(note1, "voice")
     voice.text = "1"
+    """
+    if final:
+        barline = SubElement(measure, "barline")
+        barline.attrib = {"location": "right"}
+        bar_style = SubElement(barline, "bar-style")
+        bar_style.text = "light-heavy"
 
-    barline = SubElement(measure1, "barline")
-    barline.attrib = {"location": "right"}
-    bar_style = SubElement(barline, "bar-style")
-    bar_style.text = "light-heavy"
+
+def add_part_to_xml(xml):
+
+    part = SubElement(xml, 'part')
+    part.attrib = {"id": "P1"}
+    add_measure(part, 1)
+    add_measure(part, 2)
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the piano index.")
@@ -46,12 +74,12 @@ def index(request):
 
 def page_test(request):
 
-    top = create_xml_stub()
-    add_measure_to_xml(top) 
+    notation = create_xml_stub()
+    add_part_to_xml(notation)
 
-    dom = xml.dom.minidom.parseString(tostring(top))
+    dom = xml.dom.minidom.parseString(tostring(notation))
     pretty_xml_as_string = dom.toprettyxml()
 
-    html = f'<textarea rows="20" cols="100"> {pretty_xml_as_string} </textarea>'
+    html = f'<textarea rows="20" cols="100">{pretty_xml_as_string}</textarea>'
 
     return HttpResponse(html)
