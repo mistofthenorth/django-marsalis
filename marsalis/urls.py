d8021722a2cdb1681fc8d9413dbd5094829cf26e
Brian Owen <briano.bend@icloud.com>
2021-01-20 15:54:24 -0800

    Cleaning up the view and initial url

--- marsalis/urls.py
+++ marsalis/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('piano/', include('piano.urls')),
+    path('', include('piano.urls')),
 ]
--- piano/views.py
+++ piano/views.py
@@ -6,76 +6,8 @@ import random
 from . import xmltranslate
 from . import music
 
-vex_flow = """
-
-<html>
-<script src="https://unpkg.com/vexflow/releases/vexflow-min.js"></script>
-<body>
-<div id="boo">Test</div>
-<script>
-
-VF = Vex.Flow;
-
-// Create an SVG renderer and attach it to the DIV element named "boo".
-var div = document.getElementById("boo")
-var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
-
-// Configure the rendering context.
-renderer.resize(500, 500);
-var context = renderer.getContext();
-context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
-
-// Create a stave of width 400 at position 10, 40 on the canvas.
-var stave = new VF.Stave(10, 40, 400);
-
-// Add a clef and time signature.
-stave.addClef("treble").addTimeSignature("4/4");
-
-// Connect it to the rendering context and draw!
-stave.setContext(context).draw();
-
-
-var notes = [
-  // A quarter-note C.
-  new VF.StaveNote({clef: "treble", keys: ["c/4"], duration: "q" }),
-
-  // A quarter-note D.
-  new VF.StaveNote({clef: "treble", keys: ["d/4"], duration: "q" }),
-
-  // A quarter-note rest. Note that the key (b/4) specifies the vertical
-  // position of the rest.
-  new VF.StaveNote({clef: "treble", keys: ["b/4"], duration: "qr" }),
-
-  // A C-Major chord.
-  new VF.StaveNote({clef: "treble", keys: ["c/4", "e/4", "g/4"], duration: "q" })
-];
-
-// Create a voice in 4/4 and add the notes from above
-var voice = new VF.Voice({num_beats: 4,  beat_value: 4});
-voice.addTickables(notes);
-
-// Format and justify the notes to 400 pixels.
-var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
-
-// Render voice
-voice.draw(context, stave);
-
-
-</script>
-
-</body>
-
-</html>
-
-
-"""
-
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the piano index.")
-
-
-def page_test(request):
 
     my_phrase = music.Phrase(4)
 
