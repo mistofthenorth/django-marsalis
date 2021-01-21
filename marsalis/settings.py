36cf16dd4f0d4dfab8d3d58baa2547b26ece3f6c
Brian Owen <briano.bend@icloud.com>
2021-01-21 15:56:11 -0800

    Switching to template

--- marsalis/settings.py
+++ marsalis/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'marsalis.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['piano/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- piano/views.py
+++ piano/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from xml.etree.ElementTree import Element, SubElement, Comment, tostring, ElementTree
 import xml.dom.minidom
 import random
@@ -20,40 +21,7 @@ def index(request):
         for notes in measures.notes:
             notes_list.append(notes.pitch_name[:3])
 
-    vex_flow = f"""
 
-    <html>
-    <script src="https://unpkg.com/vexflow/releases/vexflow-min.js"></script>
-    <body>
-    <div id="boo"></div>
-    <script>
-
-    const VF = Vex.Flow;
-
-    // Create an SVG renderer and attach it to the DIV element named "boo".
-    var vf = new VF.Factory({{renderer: {{elementId: 'boo'}}}});
-    var score = vf.EasyScore();
-    var system = vf.System();
-
-    system.addStave({{
-      voices: [score.voice(score.notes('{notes_list[0]}/q, {notes_list[1]}, {notes_list[2]}, {notes_list[3]}'))]
-    }}).addClef('treble').addTimeSignature('4/4');
-
-    system.addStave({{
-      voices: [score.voice(score.notes('{notes_list[4]}/q, {notes_list[5]}, {notes_list[6]}, {notes_list[7]}'))]
-    }}).addClef('treble').addTimeSignature('4/4');
-
-    vf.draw();
-
-
-
-    </script>
-
-    </body>
-
-    </html>
-
-
-    """
-
-    return HttpResponse(vex_flow)
+    template = loader.get_template('piano/index.html')
+    context = {'test' : 'This is my context text'}
+    return HttpResponse(template.render(context, request))
